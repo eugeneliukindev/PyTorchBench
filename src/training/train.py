e73ebb18f359d068e30d708900096959bdba1db7
Eugene Liukin <eugeneliukindev@gmail.com>
2025-04-07 16:27:16 +0300

    changed var starting_epoch in run_train func

--- src/training/train.py
+++ src/training/train.py
@@ -4,28 +4,31 @@ from typing import Generator, Annotated
 from torch.nn import Module
 from torch.optim import Optimizer
 from torch.optim.lr_scheduler import LRScheduler, ReduceLROnPlateau, StepLR
+from torch.utils.data import DataLoader
 from torchmetrics import Metric
 from tqdm import tqdm
+from typing_extensions import TypeVar
 
-from src.data import TensorDataloader
 from .engine import run_engine_step
 from .metrics import TrainMetrics, ValidationMetrics
 
 log = logging.getLogger(__name__)
+
+T = TypeVar("T")
 
 type SchedulerType = LRScheduler | ReduceLROnPlateau | StepLR
 
 
 def _validate_train(
     epochs: Annotated[int, "> 0"],
-    start_epoch: Annotated[int, ">= 0"],
+    starting_epoch: Annotated[int, ">= 0"],
     scheduler: SchedulerType,
     progress: bool | None = True,
 ) -> None:
     if not isinstance(epochs, int) or not epochs > 0:
         raise ValueError("epochs must be a positive integer")
-    if not isinstance(start_epoch, int) or not start_epoch >= 0:
-        raise ValueError("start_epoch must be a positive integer")
+    if not isinstance(starting_epoch, int) or not starting_epoch >= 1:
+        raise ValueError("starting_epoch must be an integer and greater than or equal to 1")
     if not isinstance(scheduler, (LRScheduler, ReduceLROnPlateau, StepLR)):
         raise TypeError("scheduler must be a LRScheduler, ReduceLROnPlateau or StepLR")
     if not isinstance(progress, bool):
@@ -35,28 +38,28 @@ def _validate_train(
 def run_train(
     model: Module,
     epochs: Annotated[int, "> 0"],
-    train_dataloader: TensorDataloader,
-    val_dataloader: TensorDataloader,
+    train_dataloader: DataLoader[T],
+    val_dataloader: DataLoader[T],
     optimizer: Optimizer,
     criterion: Module,
     accuracy_metrics: Metric,
     scheduler: SchedulerType,
-    start_epoch: Annotated[int, ">= 0"] = 0,
+    starting_epoch: Annotated[int, ">= 1"] = 1,
     progress: bool = True,
 ) -> Generator[tuple[TrainMetrics, ValidationMetrics], None, None]:
     _validate_train(
         epochs=epochs,
-        start_epoch=start_epoch,
+        starting_epoch=starting_epoch,
         scheduler=scheduler,
         progress=progress,
     )
     log.info("Starting training")
-    for epoch in range(start_epoch, epochs):
-        train_iterable: tqdm[TensorDataloader] | TensorDataloader = (
+    for epoch in range(starting_epoch, epochs + 1):
+        train_iterable: tqdm[DataLoader[T]] | DataLoader[T] = (
             tqdm(
                 train_dataloader,
                 total=len(train_dataloader),
-                desc=f"Epoch {epoch + 1}/{epochs} Train",
+                desc=f"Epoch {epoch}/{epochs} Train",
                 leave=True,
                 colour="white",
             )
@@ -73,7 +76,7 @@ def run_train(
         )
         train_metrics = TrainMetrics(epoch=epoch, **train_engine_metrics.model_dump())
 
-        val_iterable: tqdm[TensorDataloader] | TensorDataloader = (
+        val_iterable: tqdm[DataLoader[T]] | DataLoader[T] = (
             tqdm(
                 val_dataloader,
                 total=len(val_dataloader),
